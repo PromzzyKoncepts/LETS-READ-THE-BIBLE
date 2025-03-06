f60c2b01e4a7b76ba0f57d9fdf629a65b937660f
Promise Okechukwu <okechukwupromise638@gmail.com>
2025-03-06 17:25:31 +0100

    chnage baseurl

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get(`/api/videos`);
+        const response = await axios.get(`${baseUrl}/api/videos`);
         if (response.status !== 200) {
           throw new Error("Failed to fetch videos");
         }
