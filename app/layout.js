4def232e6a36a39adc11e45cb5d26cbb473a0752
Promise Okechukwu <okechukwupromise638@gmail.com>
2025-03-19 15:09:49 +0100

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,24 @@
-import Script from "next/script";
 import "./globals.css";
 import Header from "./components/Header";
 import BottomBar from "@/app/components/MobileNav";
 
-
 export const metadata = {
   title: "Lets Read the Bible Campaign",
   description:
-    "Join millions of kids and youths all over the world to read the bible with lets read the bible campaign for kids and teens of all ages. Watch fellow teens get enriched through the wisdom of the scriptures. A campaign under our Highly Esteemed Man of God, Pastor Chris Oyakhilome DSC, DD",
+    "Join millions of kids and youths worldwide in Lets read the bible, a Loveworld children’s website, featuring engaging kiddies’ cartoons, animations, and Bible-based programs. Explore the Let’s Read the Bible campaign, designed for kids and teens of all ages, and discover the wisdom of the scriptures through fun and interactive content. A transformative initiative under the guidance of our Highly Esteemed Man of God, Pastor Chris Oyakhilome DSC, DD.",
+    icons: {
+      icon: "/readthebible.ico", 
+    },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-<link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Jua&family=Lexend:wght@100..900&family=Luckiest+Guy&family=Modak&family=Sniglet&family=Manrope:wght@200..800&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="icon" href="/public/readthebible.ico"/>
+        <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Jua&family=Lexend:wght@100..900&family=Luckiest+Guy&family=Modak&family=Sniglet&family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
 
       </head>
       <body
@@ -25,8 +27,7 @@ export default function RootLayout({ children }) {
         <canvas id="world" width="0" height="0"></canvas>
         <Header />
         {children}
-        <div className="fixed z-[999] block md:hidden bottom-0"><BottomBar/></div>
-        {/* <Script src="./utils/PageAnimation.js" strategy="afterInteractive" /> */}
+        <div className="fixed z-[999] block md:hidden bottom-0"><BottomBar /></div>
       </body>
     </html>
   );
