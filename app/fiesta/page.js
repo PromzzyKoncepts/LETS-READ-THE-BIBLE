53a46697c66512ab1282e63029c860c4ac556b8b
Incredible-Mack <mackemma96@gmail.com>
2025-09-27 08:28:38 +0100

    added link to banner

--- app/fiesta/page.js
+++ app/fiesta/page.js
@@ -289,14 +289,17 @@ const Page = () => {
       </div>
 
       <div className="bg-gradien t-to-b bg-slate-400 mt-10 from-[#EBD7D2] to-white">
-        <Image
-          src="/images/LBRF-SEPT-Banner.png"
-          alt="sponsor LBR"
-          className="mx-auto md:py-10 md:w-[85%]"
-          width={1000}
-          height={1000}
-        />
+        <Link href="/lbrf">
+          <Image
+            src="/images/LBRF-SEPT-Banner.png"
+            alt="sponsor LBR"
+            className="mx-auto md:py-10 md:w-[85%]"
+            width={1000}
+            height={1000}
+          />
+        </Link>
       </div>
+
       <div className="hidden">
         <div className="md:flex items-center hidden gap-4 justify-center my-10">
           {card.map((item) => (
