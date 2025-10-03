871d92e6c637058bc023732f06bd2bebc71f4f37
Incredible-Mack <mackemma96@gmail.com>
2025-10-03 15:35:16 +0100

    adding lovetoons domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,22 @@
 const nextConfig = {
   webpack: (config) => {
     config.externals.push({
-      'socket.io-client': 'socket.io-client',
+      "socket.io-client": "socket.io-client",
     });
     return config;
   },
   experimental: {
     serverActions: {
-      bodySizeLimit: '100mb',
+      bodySizeLimit: "100mb",
     },
   },
   images: {
-    domains: ["res.cloudinary.com", "d1ent1.loveworldcloud.com"], // Add the Cloudinary domain here
+    domains: [
+      "res.cloudinary.com",
+      "d1ent1.loveworldcloud.com",
+      "lovetoons.org",
+      "lovetoons.tv",
+    ], // Add the Cloudinary domain here
   },
   async headers() {
     return [
@@ -22,12 +27,19 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
