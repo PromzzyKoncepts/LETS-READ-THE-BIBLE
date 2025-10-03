52226c01f9ad412f88c670c0d9aad8c284921448
Incredible-Mack <mackemma96@gmail.com>
2025-10-03 17:01:38 +0100

    ddm

--- app/api/generate-avatar/route.js
+++ app/api/generate-avatar/route.js
@@ -43,7 +43,7 @@ export async function POST(req) {
     userImage.resize({ w: 272, h: 272 }); // Resize user's image
 
     // Merge images (overlay user image on avatar)
-    avatar.composite(userImage, 130, 82.5);
+    avatar.composite(userImage, 130, 82);
 
     const mergedImageBase64 = await avatar.getBase64(JimpMime.png, {
       quality: 50,
