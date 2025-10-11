33bb7aa236e958395f5020d36601115891523dcd
Incredible-Mack <mackemma96@gmail.com>
2025-10-11 12:45:37 +0100

    checking live

--- app/live/page.js
+++ app/live/page.js
@@ -1,14 +1,14 @@
-"use client";
-import Image from "next/image";
+// "use client";
+// import Image from "next/image";
 
-const Live = () => {
-  const router = useRouter();
+// const Live = () => {
+//   const router = useRouter();
 
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
+//   );
+// };
 
-export default Live;
+// export default Live;
