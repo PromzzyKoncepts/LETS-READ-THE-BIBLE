cfd77dcb97a8823f80f74cfbb339ef3d8e7c32ff
Incredible-Mack <mackemma96@gmail.com>
2025-10-11 10:19:08 +0100

    avatar and live page

---
+++ app/live/page.js
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+
+const Live = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Live;
