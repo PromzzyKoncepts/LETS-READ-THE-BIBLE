09f2f0e082c4fc26b4981f9c07cc20b6ed500e71
Incredible-Mack <mackemma96@gmail.com>
2025-11-07 23:28:45 +0100

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -6,27 +6,29 @@ const nextConfig = {
     });
     return config;
   },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "100mb",
     },
   },
+
   images: {
     domains: [
       "res.cloudinary.com",
       "d1ent1.loveworldcloud.com",
       "lovetoons.org",
       "lovetoons.tv",
-    ], // Add the Cloudinary domain here
+    ],
   },
+
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
@@ -37,6 +39,16 @@ const nextConfig = {
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
+      },
+    ];
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Match all routes
+        destination: "https://lovetoons.org/lbrf", // Fixed redirect target
+        permanent: false, // 307 Temporary redirect
       },
     ];
   },
