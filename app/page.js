a796b86d11686aa929fce1edcc3cd980bc673bc3
Incredible-Mack <mackemma96@gmail.com>
2026-01-12 14:34:22 +0100

    New

--- app/page.js
+++ app/page.js
@@ -42,11 +42,11 @@ export default function Home() {
   }, []);
 
   const card = [
-    // {
-    //   title: "Lovetoons Bible reading fiesta",
-    //   src: "/images/LBRF-SEPT-Banner-1.png",
-    //   link: "/lbrf",
-    // },
+    {
+      title: "Year of Manifestation",
+      src: "https://lovetoons.org/img/YEAR_OF_MANIFESTATION_web2.png",
+      // link: "/lbrf",
+    },
     {
       title: "Tick Talk",
       src: "/images/ticktalk.png",
